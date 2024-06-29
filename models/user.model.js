8b0075ddd45b68d0b0e8225d4fca7cf6ecfc25e2
Tranggg1210 <nguyenthitrang.ttd@gmail.com>
2024-06-29 09:28:11 +0700

    ✨ complete user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
-const { required } = require('joi');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { USER_ROLE_ENUM, SALT_WORK_FACTOR } = require('../constants');
 
 const Schema = mongoose.Schema;
 
@@ -22,8 +23,40 @@ const userSchema = new Schema(
             required: true,
         },
         dateOfBirth: {
-        type: Date,
-        default: '2000-01-01',
+            type: Date,
+            default: '2000-01-01',
         },
+        isLock: {
+            type: Boolean,
+            default: false
+        },
+        avatar: {
+            type: String,
+            trim: true,
+            default: "https://th.bing.com/th/id/OIP.z3fa8PjEnvzg4bhW61tEOwAAAA?rs=1&pid=ImgDetMain"
+        },
+        role:{
+            type: String,
+            enum: USER_ROLE_ENUM,
+            default: USER_ROLE_ENUM.MEMBER
+        }
+    },
+    {timestamps: true}
+);
+
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(user.isModified('password'))
+    {
+        user.password = await bcrypt.hash(user.password, SALT_WORK_FACTOR);
     }
-)
+    next();
+});
+
+userSchema.methods.isMatchPassword = async function(password){
+    const user = this;
+    return await bcrypt.compare(password, user.password);
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
