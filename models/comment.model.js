0708acb03b288e4b4e1927792de7bd18669182fa
Tranggg1210 <nguyenthitrang.ttd@gmail.com>
2024-06-29 10:07:52 +0700

    ✨ complete comment model

---
+++ models/comment.model.js
@@ -0,0 +1,36 @@
+const { required, number } = require('joi');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    image: {
+      type: String,
+    },
+    star: {
+      type: Number,
+      default: 0,
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true,
+    },
+    user: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
--- models/user.model.js
+++ models/user.model.js
@@ -5,62 +5,61 @@ const { USER_ROLE_ENUM, SALT_WORK_FACTOR } = require('../constants');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-    {
-        email: {
-            type: String,
-            trim: true,
-            unique: true,
-            required: true
-        },
-        fullName:{
-            type: String,
-            required: true,
-            trim: true
-        },
-        password: {
-            type: String,
-            select: false,
-            required: true,
-        },
-        dateOfBirth: {
-            type: Date,
-            default: '2000-01-01',
-        },
-        isLock: {
-            type: Boolean,
-            default: false
-        },
-        avatar: {
-            type: String,
-            trim: true,
-            default: "https://th.bing.com/th/id/OIP.z3fa8PjEnvzg4bhW61tEOwAAAA?rs=1&pid=ImgDetMain"
-        },
-        role:{
-            type: String,
-            enum: USER_ROLE_ENUM,
-            default: USER_ROLE_ENUM.MEMBER
-        },
-        star:{
-            type: Number,
-            default: 0
-        }
+  {
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
     },
-    {timestamps: true}
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      select: false,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      default: '2000-01-01',
+    },
+    isLock: {
+      type: Boolean,
+      default: false,
+    },
+    avatar: {
+      type: String,
+      trim: true,
+      default: 'https://th.bing.com/th/id/OIP.z3fa8PjEnvzg4bhW61tEOwAAAA?rs=1&pid=ImgDetMain',
+    },
+    role: {
+      type: String,
+      enum: USER_ROLE_ENUM,
+      default: USER_ROLE_ENUM.MEMBER,
+    },
+    star: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
 );
 
-userSchema.pre('save', async function(next){
-    const user = this;
-    if(user.isModified('password'))
-    {
-        user.password = await bcrypt.hash(user.password, SALT_WORK_FACTOR);
-    }
-    next();
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_WORK_FACTOR);
+  }
+  next();
 });
 
-userSchema.methods.isMatchPassword = async function(password){
-    const user = this;
-    return await bcrypt.compare(password, user.password);
-}
+userSchema.methods.isMatchPassword = async function (password) {
+  const user = this;
+  return await bcrypt.compare(password, user.password);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
